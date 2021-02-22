0c59237ad7cb48dbac89303bb385992e935f3c1e
FilippoFantinato <fantinato.filippo99@gmail.com>
2021-02-22 11:48:44 +0100

    UPDATE

--- TextToMaps/geo.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-maps = "https://www.google.it/maps/dir/"
-
-for location in sys.argv[1].split("-"):
-	maps += location.replace("P.N.", "") + "/"
-
-print(maps)
--- english-solutions/sen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-exercise_dir='exercises'
-
-for file in `ls $exercise_dir`; do
-	python english-solutions.py $exercise_dir/$file solutions
-done
